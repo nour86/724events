fa2312a33fc5f49058551644a0210c2888db257d
js <techerjeansebastien.pro@gmail.com>
2022-05-03 07:41:33 +0200

    Create the application

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+/* istanbul ignore file */
+import "./App.scss";
+import Page from "./pages/Home";
+import { DataProvider } from "./contexts/DataContext";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DataProvider>
+      <Page />
+    </DataProvider>
   );
 }
 
