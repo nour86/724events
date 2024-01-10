834f63f73567ab4a3ea286176d8c0df068774fe8
Nour Alchami <n.alchami@gmail.com>
2024-01-10 12:33:49 +0100

    added smooth behavior to scroll in app.scss

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -41,38 +41,37 @@ const Slider = () => {
     return (
         <div className="SlideCardList">
             {byDateDesc?.map((event, idx) => (
-                <>
-                    <div
-                        key={event.title}
-                        className={`SlideCard SlideCard--${
-                            index === idx ? 'display' : 'hide'
-                        }`}
-                    >
-                        <img src={event.cover} alt="forum" />
-                        <div className="SlideCard__descriptionContainer">
-                            <div className="SlideCard__description">
-                                <h3>{event.title}</h3>
-                                <p>{event.description}</p>
-                                <div>{getMonth(new Date(event.date))}</div>
-                            </div>
+                <div
+                    key={event.title}
+                    className={`SlideCard SlideCard--${
+                        index === idx ? 'display' : 'hide'
+                    }`}
+                >
+                    <img src={event.cover} alt="forum" />
+                    <div className="SlideCard__descriptionContainer">
+                        <div className="SlideCard__description">
+                            <h3>{event.title}</h3>
+                            <p>{event.description}</p>
+                            <div>{getMonth(new Date(event.date))}</div>
                         </div>
                     </div>
-                    <div className="SlideCard__paginationContainer">
-                        <div className="SlideCard__pagination">
-                            {/* radioIdx */}
-                            {byDateDesc.map((_, radioIdx) => (
-                                <input
-                                    key={`input-${radioIdx + 1}-${event.title}`}
-                                    type="radio"
-                                    name="radio-button"
-                                    checked={index === radioIdx}
-                                    readOnly
-                                />
-                            ))}
-                        </div>
-                    </div>
-                </>
+                </div>
             ))}
+
+            <div className="SlideCard__paginationContainer">
+                <div className="SlideCard__pagination">
+                    {/* radioIdx */}
+                    {byDateDesc?.map((_, radioIdx) => (
+                        <input
+                            key={`input-${radioIdx + 1}-${_.title}`}
+                            type="radio"
+                            name="radio-button"
+                            checked={index === radioIdx}
+                            readOnly
+                        />
+                    ))}
+                </div>
+            </div>
         </div>
     )
 }
